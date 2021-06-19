18a7e4664c4953008ddf11cd9642343328547f3f
Andy <andyqin17@gmail.com>
2021-06-19 14:52:04 -0700

    Add canvas and image loading

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,12 +1,8 @@
 
 import React, { useRef, useEffect } from 'react'
+import tapir from './tapir.png';
 
 function Canvas(props) {
-
-  var img = new Image(); 
-  img.addEventListener('load', function () {
-  }, false);
-  img.src = '../public/cat.png';
 
   const canvasRef = useRef(null)
   useEffect(() => {
@@ -14,10 +10,22 @@ function Canvas(props) {
     const context = canvas.getContext('2d')
     context.fillStyle = '#000000'
     context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+
+    var img = new Image();
+    img.src = tapir;
+    console.log("loaded");
+    img.addEventListener('load', function() {
+      context.drawImage(img, 0, 0);
+    }, false);
+
+
   }, [])
 
   return (
-    <canvas ref={canvasRef} {...props} />
+    <div>
+      <canvas ref={canvasRef} {...props} />
+    </div>
+
   );
 }
 
