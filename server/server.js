a7636bae47aa1bebcb348807a4cb36a3af27a942
Andy <andyqin17@gmail.com>
2021-06-22 11:37:36 -0700

    Switch to serving build

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-
+const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log('Listening on port ' + port);
+   console.log(`Listening on port ${port}`);
 });
+
